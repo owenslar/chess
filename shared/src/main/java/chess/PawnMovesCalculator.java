aebb3b76a4832006a05c08469b5fa39b3c34d2aa
Owen Larson <owenslar@byu.edu>
2025-01-21 11:52:32 -0700

    initial 2 square pawn moves implemented

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -1,11 +1,26 @@
 package chess;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class PawnMovesCalculator extends PieceMovesCalculator {
 
     public static Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>();
+        ChessPiece myPiece = board.getPiece(myPosition);
+        Collection<ChessMove> moves = new ArrayList<>();
+        int x = myPosition.getRow();
+        int y = myPosition.getColumn();
+        if (myPiece.getTeamColor() == ChessGame.TeamColor.WHITE && x == 2) {
+            ChessPosition nextPosition = new ChessPosition(x + 2, y);
+            ChessMove move = new ChessMove(myPosition, nextPosition);
+            moves.add(move);
+        }
+        else if (myPiece.getTeamColor() == ChessGame.TeamColor.BLACK && x == 7) {
+            ChessPosition nextPosition = new ChessPosition(x - 2, y);
+            ChessMove move = new ChessMove(myPosition, nextPosition);
+            moves.add(move);
+        }
+        return moves;
     }
 }
