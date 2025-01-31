81452069af97609997a0b01bbcad391f5166a2e1
Owen Larson <owenslar@byu.edu>
2025-01-30 22:17:10 -0700

    implemented KingFinder class to return position of king of whatever color

--- shared/src/main/java/chess/KingFinder.java
+++ shared/src/main/java/chess/KingFinder.java
@@ -3,20 +3,27 @@ package chess;
 public class KingFinder {
 
     ChessBoard board;
+    ChessGame.TeamColor teamColor;
 
-    public KingFinder(ChessBoard board) {
+    public KingFinder(ChessBoard board, ChessGame.TeamColor teamColor) {
         this.board = board;
+        this.teamColor = teamColor;
     }
 
-    public ChessPosition getWhiteKing() {
-
-    }
-
-    public ChessPosition getBlackKing() {
-
+    public ChessPosition getKing(ChessGame.TeamColor teamColor) {
+        return findKing(teamColor);
     }
 
     private ChessPosition findKing(ChessGame.TeamColor teamColor) {
-
+        for (int row = 1; row < 9; row++) {
+            for (int col = 1; col < 9; col++) {
+                ChessPosition currPosition = new ChessPosition(row, col);
+                ChessPiece currPiece = board.getPiece(currPosition);
+                if (currPiece.getPieceType() == ChessPiece.PieceType.KING && currPiece.getTeamColor() == teamColor) {
+                    return currPosition;
+                }
+            }
+        }
+        return null;
     }
 }
