d196efb83ca2e16c15243fb3c64be834517f0827
Owen Larson <owenslar@byu.edu>
2025-01-30 22:04:11 -0700

    added KingFinder class

---
+++ shared/src/main/java/chess/KingFinder.java
@@ -0,0 +1,22 @@
+package chess;
+
+public class KingFinder {
+
+    ChessBoard board;
+
+    public KingFinder(ChessBoard board) {
+        this.board = board;
+    }
+
+    public ChessPosition getWhiteKing() {
+
+    }
+
+    public ChessPosition getBlackKing() {
+
+    }
+
+    private ChessPosition findKing(ChessGame.TeamColor teamColor) {
+
+    }
+}
