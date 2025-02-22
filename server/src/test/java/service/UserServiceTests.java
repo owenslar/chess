2cc200855c0f5d3e12173deae9933cf71d2f6dcb
Owen Larson <owenslar@byu.edu>
2025-02-21 19:58:56 -0700

    Changed the error handling in my tests back

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -7,8 +7,6 @@ import requestresult.RegisterRequest;
 import requestresult.RegisterResult;
 import org.junit.jupiter.api.Assertions;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class UserServiceTests {
 
     private UserService userService;
@@ -20,54 +18,41 @@ public class UserServiceTests {
 
 
     @Test
-    public void positiveRegisterTest() {
-        try {
-            RegisterRequest registerRequest = new RegisterRequest("owenlarson", "foobar", "owlar23@icstudents.org");
+    public void positiveRegisterTest() throws DataAccessException {
+        RegisterRequest registerRequest = new RegisterRequest("owenlarson", "foobar", "owlar23@icstudents.org");
 
-            RegisterResult actual = userService.register(registerRequest);
+        RegisterResult actual = userService.register(registerRequest);
 
-            Assertions.assertNull(actual.message());
-            Assertions.assertNotNull(actual.authToken());
-            Assertions.assertNotNull(actual.username());
-            Assertions.assertEquals("owenlarson", actual.username());
-            Assertions.assertEquals(200, actual.statusCode());
-        } catch (DataAccessException e) {
-            fail("DataAccessException was caught");
-        }
-
+        Assertions.assertNull(actual.message());
+        Assertions.assertNotNull(actual.authToken());
+        Assertions.assertNotNull(actual.username());
+        Assertions.assertEquals("owenlarson", actual.username());
+        Assertions.assertEquals(200, actual.statusCode());
     }
 
     @Test
-    public void badRequestRegisterTest() {
-        try {
-            RegisterRequest registerRequest = new RegisterRequest(null,"password", "a@o.org");
+    public void badRequestRegisterTest() throws DataAccessException {
+        RegisterRequest registerRequest = new RegisterRequest(null,"password", "a@o.org");
 
-            RegisterResult actual = userService.register(registerRequest);
+        RegisterResult actual = userService.register(registerRequest);
 
-            Assertions.assertEquals("Error: bad request", actual.message());
-            Assertions.assertEquals(400, actual.statusCode());
-            Assertions.assertNull(actual.username());
-            Assertions.assertNull(actual.authToken());
-        } catch (DataAccessException e) {
-            fail("DataAccessException was caught");
-        }
+        Assertions.assertEquals("Error: bad request", actual.message());
+        Assertions.assertEquals(400, actual.statusCode());
+        Assertions.assertNull(actual.username());
+        Assertions.assertNull(actual.authToken());
     }
 
     @Test
-    public void registerSameUserTwiceTest() {
-        try {
-            RegisterRequest registerRequest = new RegisterRequest("owen", "foo", "o@gmail.com");
+    public void registerSameUserTwiceTest() throws DataAccessException {
+        RegisterRequest registerRequest = new RegisterRequest("owen", "foo", "o@gmail.com");
 
-            userService.register(registerRequest);
+        userService.register(registerRequest);
 
-            RegisterResult actual = userService.register(registerRequest);
+        RegisterResult actual = userService.register(registerRequest);
 
-            Assertions.assertEquals("Error: already taken", actual.message());
-            Assertions.assertEquals(403, actual.statusCode());
-            Assertions.assertNull(actual.username());
-            Assertions.assertNull(actual.authToken());
-        } catch (DataAccessException e) {
-            fail("DataAccessException was caught");
-        }
+        Assertions.assertEquals("Error: already taken", actual.message());
+        Assertions.assertEquals(403, actual.statusCode());
+        Assertions.assertNull(actual.username());
+        Assertions.assertNull(actual.authToken());
     }
 }
