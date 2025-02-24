bc28f849c3a018e04aa3e80984d4d0af3fb4d315
Owen Larson <owenslar@byu.edu>
2025-02-23 20:33:58 -0700

    LoginHandler implemented, UserService needs to return the correct object

--- server/src/main/java/handler/LoginHandler.java
+++ server/src/main/java/handler/LoginHandler.java
@@ -1,13 +1,25 @@
 package handler;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import dataaccess.DataAccessException;
+import requestresult.LoginRequest;
+import requestresult.LoginResult;
+import service.UserService;
 import spark.Request;
 import spark.Response;
 
 public class LoginHandler extends BaseHandler {
+    private final UserService userService = new UserService();
+
     @Override
     protected Object processRequest(Request req, Response res, String authToken) throws DataAccessException {
-        return null;
+        LoginRequest loginRequest = new Gson().fromJson(req.body(), LoginRequest.class);
+        LoginResult loginResult = userService.login(loginRequest);
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        res.status(loginResult.statusCode());
+        return gson.toJson(loginResult);
+
     }
 
     @Override
