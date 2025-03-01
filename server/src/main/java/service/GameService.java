c2e035f22ad7f46b6664f6b5adc384dd2e442e6e
Owen Larson <owenslar@byu.edu>
2025-02-28 20:04:53 -0700

    join method implemented on GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,22 +1,23 @@
 package service;
 
 import chess.ChessGame;
+import dataaccess.AuthDAO;
 import dataaccess.DaoFactory;
 import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
 import dtos.GameSummary;
+import model.AuthData;
 import model.GameData;
-import requestresult.CreateRequest;
-import requestresult.CreateResult;
-import requestresult.ListRequest;
-import requestresult.ListResult;
+import requestresult.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class GameService {
 
     GameDAO gameDAO = DaoFactory.createGameDAO();
+    AuthDAO authDAO = DaoFactory.createAuthDAO();
 
     public CreateResult create(CreateRequest r) throws DataAccessException {
         // 1. Verify the input
@@ -54,5 +55,49 @@ public class GameService {
         // 4. Create a ListResult object that contains the list of games and correct status code and return it
         return new ListResult(summaries, null, 200);
 
+    }
+
+    public JoinResult join(JoinRequest r) throws DataAccessException {
+        // They will already be authenticated
+        // 1. Verify the input, (verify the playerColor is either WHITE or BLACK and not empty or null),
+        // return a bad request error if not
+        if (!Objects.equals(r.playerColor(), "WHITE") && !Objects.equals(r.playerColor(), "BLACK")) {
+            return new JoinResult("Error: bad request", 400);
+        }
+        // 2. Verify that the game exists, return a bad request error if not
+        GameData requestedGameData = gameDAO.getGame(r.gameID());
+        if (requestedGameData == null) {
+            return new JoinResult("Error: bad request", 400);
+        }
+
+        // 3. If the game exists, make sure the requested color is not already taken (return 403 if it is)
+        GameData newGameData;
+        AuthData callerAuthData = authDAO.getAuth(r.authToken());
+
+        if (r.playerColor().equals("WHITE")) {
+            if (requestedGameData.whiteUsername() != null) {
+                return new JoinResult("Error: already taken", 403);
+            }
+            newGameData = new GameData(requestedGameData.gameID(),
+                    callerAuthData.username(),
+                    requestedGameData.blackUsername(),
+                    requestedGameData.gameName(),
+                    requestedGameData.game());
+            gameDAO.updateGame(newGameData);
+        }
+        else {
+            if (requestedGameData.blackUsername() != null) {
+                return new JoinResult("Error: already taken", 403);
+            }
+            newGameData = new GameData(requestedGameData.gameID(),
+                    requestedGameData.whiteUsername(),
+                    callerAuthData.username(),
+                    requestedGameData.gameName(),
+                    requestedGameData.game());
+            gameDAO.updateGame(newGameData);
+        }
+
+        // 5. Create a join result object and return it
+        return new JoinResult(null, 200);
     }
 }
