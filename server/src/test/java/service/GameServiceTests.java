38647cbbb1cc9053db76aea005a8130533f7720e
Owen Larson <owenslar@byu.edu>
2025-02-28 19:22:23 -0700

    list GameServiceTests written and passing

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -4,20 +4,20 @@ import dataaccess.DataAccessException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import requestresult.CreateRequest;
-import requestresult.CreateResult;
-import requestresult.RegisterRequest;
-import requestresult.RegisterResult;
+import requestresult.*;
 
 public class GameServiceTests {
 
     GameService gameService;
     UserService userService;
+    ClearService clearService;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws DataAccessException {
         userService = new UserService();
         gameService = new GameService();
+        clearService = new ClearService();
+        clearService.clear();
     }
 
     @Test
@@ -44,5 +44,38 @@ public class GameServiceTests {
         Assertions.assertNull(createResult.gameID());
         Assertions.assertEquals("Error: bad request", createResult.message());
         Assertions.assertEquals(400, createResult.statusCode());
+    }
+
+    @Test
+    public void positiveListTest() throws DataAccessException {
+        RegisterRequest registerRequest = new RegisterRequest("username", "password", "foor@bar");
+        RegisterResult registerResult = userService.register(registerRequest);
+
+        CreateRequest createRequest = new CreateRequest("newGame", registerResult.authToken());
+        gameService.create(createRequest);
+
+        ListRequest listRequest = new ListRequest(registerResult.authToken());
+        ListResult listResult = gameService.list(listRequest);
+
+        Assertions.assertNotNull(listResult.games());
+        Assertions.assertEquals("newGame", listResult.games().getFirst().gameName());
+        Assertions.assertEquals(200, listResult.statusCode());
+        Assertions.assertNull(listResult.message());
+    }
+
+    @Test
+    public void emptyAuthTokenListRequestTest() throws DataAccessException {
+        RegisterRequest registerRequest = new RegisterRequest("username", "password", "foor@bar");
+        RegisterResult registerResult = userService.register(registerRequest);
+
+        CreateRequest createRequest = new CreateRequest("newGame", registerResult.authToken());
+        gameService.create(createRequest);
+
+        ListRequest listRequest = new ListRequest("");
+        ListResult listResult = gameService.list(listRequest);
+
+        Assertions.assertNull(listResult.games());
+        Assertions.assertEquals(401, listResult.statusCode());
+        Assertions.assertEquals("Error: unauthorized", listResult.message());
     }
 }
