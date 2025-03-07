59cca216a7a1aebf26cc43c36a8c3b57d104679b
Owen Larson <owenslar@byu.edu>
2025-03-07 00:08:51 -0700

    added UserDAO tests file, still need to write tests

---
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -0,0 +1,17 @@
+//package dataaccess;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//public class UserDAOTests {
+//
+//    UserDAO userDAO;
+//
+//    @BeforeEach
+//    public void setUp() {
+//        userDAO = DaoFactory.createUserDAO();
+//    }
+//
+//    @Test
+//    public void
+//}
