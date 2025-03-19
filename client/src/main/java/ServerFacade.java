79f882439e708caf7846bd3d66183b644a0eecf4
Owen Larson <owenslar@byu.edu>
2025-03-18 22:42:24 -0600

    moved Request and Result classes (and dto class) to shared module so that they can be used by the client

---
+++ client/src/main/java/ServerFacade.java
@@ -0,0 +1,10 @@
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) { serverUrl = url; }
+
+    public RegisterResult register(RegisterRequest request) {
+
+    }
+}
