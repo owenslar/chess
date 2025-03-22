cffd447a09b98092a403cd6ab49dbdaf1ef22fb5
Owen Larson <owenslar@byu.edu>
2025-03-22 11:10:26 -0600

    basic files set up for UI

---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class Repl {
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient(serverUrl);
+    }
+
+    public void run() {
+
+    }
+}
--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,4 +1,4 @@
-import chess.*;
+
 import dataaccess.AuthDAO;
 import dataaccess.DaoFactory;
 import dataaccess.GameDAO;
@@ -9,8 +9,6 @@ import service.UserService;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
 
         UserDAO userDAO = DaoFactory.createUserDAO();
         AuthDAO authDAO = DaoFactory.createAuthDAO();
