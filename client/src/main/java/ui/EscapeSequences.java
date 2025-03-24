1183df8cae70cd8d93f637cfeef8ea60d8310574
Owen Larson <owenslar@byu.edu>
2025-03-23 22:51:56 -0600

    test row printing out with correct formatting

--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -33,6 +33,7 @@ public class EscapeSequences {
     public static final String SET_TEXT_COLOR_BLUE = SET_TEXT_COLOR + "12m";
     public static final String SET_TEXT_COLOR_MAGENTA = SET_TEXT_COLOR + "5m";
     public static final String SET_TEXT_COLOR_WHITE = SET_TEXT_COLOR + "15m";
+    public static final String SET_TEXT_COLOR_DARK_GREEN = SET_TEXT_COLOR + "22m";
     public static final String RESET_TEXT_COLOR = UNICODE_ESCAPE + "[39m";
 
     public static final String SET_BG_COLOR_BLACK = SET_BG_COLOR + "0m";
--- client/src/main/java/ui/GameplayClient.java
+++ client/src/main/java/ui/GameplayClient.java
@@ -38,16 +38,29 @@ public class GameplayClient {
             letters = new String[]{"h","g","f","e","d","c","b","a"};
         }
         appendLettersRow(result, letters);
-        result.append("REST OF THE CHESS BOARD PRINTED HERE\n");
+        appendTestRow(result);
         appendLettersRow(result, letters);
         return result.toString();
     }
 
     private void appendLettersRow(StringBuilder result, String[] letters) {
-        result.append(SET_BG_COLOR_BLUE + SET_TEXT_COLOR_BLUE + "   ");
+        result.append(SET_BG_COLOR_DARK_GREY + SET_TEXT_COLOR_DARK_GREY + "   ");
         for (String letter : letters) {
-            result.append("  " + RESET_TEXT_COLOR).append(letter).append(SET_TEXT_COLOR_BLUE).append(" ");
+            result.append("  " + SET_TEXT_COLOR_WHITE).append(letter).append(SET_TEXT_COLOR_DARK_GREY).append("  ");
         }
         result.append("   ").append(RESET_BG_COLOR).append("\n");
+    }
+
+    private void appendTestRow(StringBuilder result) {
+        result.append(SET_BG_COLOR_DARK_GREY + "   ");
+        for (int i = 0; i < 8; i++) {
+            if (i % 2 == 0) {
+                result.append(SET_BG_COLOR_DARK_GREEN + SET_TEXT_COLOR_DARK_GREEN).append(" ").append(EMPTY).append(" ");
+            } else {
+                result.append(SET_BG_COLOR_WHITE + SET_TEXT_COLOR_WHITE).append(" ").append(EMPTY).append(" ");
+            }
+        }
+        result.append(SET_BG_COLOR_DARK_GREY + "   ");
+        result.append(RESET_BG_COLOR).append("\n");
     }
 }
