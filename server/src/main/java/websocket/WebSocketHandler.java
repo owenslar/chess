55851d0fc1c52f42460573b82f5398f073eec5f1
Owen Larson <owenslar@byu.edu>
2025-03-31 21:49:21 -0600

    connect and leave websocket methods working

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -17,6 +17,7 @@ import websocket.commands.UserGameCommand;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Set;
 
 import static websocket.commands.UserGameCommand.CommandType.*;
@@ -66,47 +67,108 @@ public class WebSocketHandler {
                         resign(session, commandObject);
                         break;
                     default:
-                        sendMessage(session, new ErrorMessage("Invalid command"));
+                        sendMessage(session, new ErrorMessage("Error: Invalid command"));
                 }
             }
 
         } catch(IllegalArgumentException e) {
-            sendMessage(session, new ErrorMessage("Invalid message format"));
+            sendMessage(session, new ErrorMessage("Error: Invalid message format"));
         }
     }
 
     private void connect(Session session, UserGameCommand commandObject) throws DataAccessException, IOException {
         // SKIP THE AUTHENTICATION FOR NOW BUT MAYBE ADD IT LATER
-        // Authenticate user
+        // 1. Authenticate user and add them to sessions
+        if (commandObject.getAuthToken() == null) {
+            sendMessage(session, new ErrorMessage("Error: no AuthToken provided"));
+            return;
+        }
+        if (commandObject.getCommandType() == null) {
+            sendMessage(session, new ErrorMessage("Error: no GameID provided"));
+            return;
+        }
         AuthData authData = authDAO.getAuth(commandObject.getAuthToken());
         if (authData == null) {
             sendMessage(session, new ErrorMessage("Error: unauthorized"));
             return;
         }
-        // 1. Make sure the user is authenticated and add them to your sessions collection for the correct game
         sessions.addSessionToGame(commandObject.getGameID(), session);
 
-        // 1. Send a LOAD_GAME message to current session
+        // 2. Send a LOAD_GAME message to current session
         GameData currentGame = gameDAO.getGame(commandObject.getGameID());
         if (currentGame == null) {
-            sendMessage(session, new ErrorMessage("Error: Invalid GameID"));
+            sendMessage(session, new ErrorMessage("Error: game not found"));
             return;
         }
         LoadGameMessage loadGameMessage = new LoadGameMessage(currentGame.game());
         sendMessage(session, loadGameMessage);
 
-        // 2. Send a NOTIFICATION message to all OTHER clients connected to that game that someone joined
-        NotificationMessage notificationMessage = new NotificationMessage(authData.username() + " joined the game!");
+        // 3. Send a NOTIFICATION message to all OTHER clients connected to that game that someone joined including what role
+        // and their username
+        NotificationMessage notificationMessage = getNotificationMessage(currentGame, authData);
         broadcastMessage(commandObject.getGameID(), notificationMessage, session);
-        // remember to use their username in the message
+    }
+
+    private NotificationMessage getNotificationMessage(GameData currentGame, AuthData authData) {
+        NotificationMessage notificationMessage;
+        boolean whiteUser = Objects.equals(currentGame.whiteUsername(), authData.username());
+        boolean blackUser = Objects.equals(currentGame.blackUsername(), authData.username());
+        if (whiteUser && blackUser) {
+            notificationMessage = new NotificationMessage(authData.username() + " is now playing as both players");
+        } else if (whiteUser) {
+            notificationMessage = new NotificationMessage(authData.username() + " is now playing as WHITE");
+        } else if (blackUser) {
+            notificationMessage = new NotificationMessage(authData.username() + " is now playing as BLACK");
+        } else {
+            notificationMessage = new NotificationMessage(authData.username() + " is now observing the game");
+        }
+        return notificationMessage;
     }
 
     private void makeMove(Session session, UserGameCommand commandObject) {
 
     }
 
-    private void leave(Session session, UserGameCommand commandObject) {
+    private void leave(Session session, UserGameCommand commandObject) throws IOException, DataAccessException {
+        // 1. Authenticate the user and validate the command (check if the return types are null)
+        if (commandObject.getAuthToken() == null) {
+            sendMessage(session, new ErrorMessage("Error: no authToken provided"));
+            return;
+        }
+        if (commandObject.getGameID() == null) {
+            sendMessage(session, new ErrorMessage("Error: no GameID provided"));
+            return;
+        }
+        AuthData authData = authDAO.getAuth(commandObject.getAuthToken());
+        if (authData == null) {
+            sendMessage(session, new ErrorMessage("Error: unauthorized"));
+            return;
+        }
 
+        // 2. Retrieve the game from the db, update it by removing the player, update it in the DB
+        GameData gameData = gameDAO.getGame(commandObject.getGameID());
+        if (gameData == null) {
+            sendMessage(session, new ErrorMessage("Error: game not found"));
+            return;
+        }
+        if (Objects.equals(gameData.whiteUsername(), authData.username())) {
+            GameData newGameData = new GameData(gameData.gameID(), null,
+                    gameData.blackUsername(), gameData.gameName(), gameData.game());
+            gameDAO.updateGame(newGameData);
+        } else if (Objects.equals(gameData.blackUsername(), authData.username())) {
+            GameData newGameData = new GameData(gameData.gameID(), gameData.whiteUsername(),
+                    null, gameData.gameName(), gameData.game());
+            gameDAO.updateGame(newGameData);
+        } else {
+            broadcastMessage(commandObject.getGameID(), new NotificationMessage(authData.username() + " is no longer observing the game"), session);
+            return;
+        }
+
+        // 3. Send a Notification object to all other clients in the game
+        // informing them that the user left (both players and observers)
+        NotificationMessage notificationMessage = new NotificationMessage(authData.username() + " left the game");
+        broadcastMessage(commandObject.getGameID(), notificationMessage, session);
+        sessions.removeSessionFromGame(commandObject.getGameID(), session);
     }
 
     private void resign(Session session, UserGameCommand commandObject) {
